603a7c3b838fb7b6676b73de11122b647256ef1a
JSatterf1579 <joseph.satterfield@case.edu>
2013-02-17 21:43:02 -0500

    Scene Changes:   Reattached script to GameMaster
    GameMaster.cs changes:
      Added more comments
      Moved paddle wobble functionality to a new method
      Created method for camera shake
      Added more levels of intoxication
      Fixed a bug where the blocks would rotate too far
    
    Signed-off-by: JSatterf1579 <joseph.satterfield@case.edu>

--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -1,43 +1,102 @@
+/**
+ * Methods that control the general background functionality of the game
+ * Author: Joseph Satterfield
+ **/
+
 using UnityEngine;
 using System.Collections;
 
 public class GameMaster : MonoBehaviour {
 	
 	private GameObject[] wobbleBlocks;
-	private float score = 0.08f;
+	public float score = 0.16f;
 	private bool angle = true;
 	private float ang = 0;
+	private GameObject mainCamera;
+	private float distance = 0;
+	private bool direction = true;
 	
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("Running");
 		wobbleBlocks = GameObject.FindGameObjectsWithTag("wobble");
+		mainCamera = GameObject.Find ("Main Camera");
 		Debug.Log(wobbleBlocks.Length);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if(score >= 0.08f)
+		if(score >= 0.08f && score < 0.16f)
 		{
-			Debug.Log (score);
-			//Looks at each of the blocks that can wobble
-			foreach(GameObject go in wobbleBlocks)	
+			TurnPaddles (1.5f, 1.0f);
+		}
+		
+		if(score >= 0.16f && score < 0.24f)
+		{
+			TurnPaddles (3.0f, 1.2f);
+			ShakeCamera (0.25f, 0.5f);
+		}
+		
+		if(score >= 0.24f)
+		{
+			TurnPaddles (4.0f, 1.3f);
+			ShakeCamera (0.25f, 0.9f);
+		}
+	}
+	
+	///<summary>
+	///Turns the blocks to a specified angle at a specified speed
+	///</summary>
+	///<param name = "maxAngle">
+	///The maximum angle that the blocks can turn to
+	///</param>
+	///<param name="speed">
+	///How fast the platforms move
+	///</param>>
+	void TurnPaddles (float maxAngle, float speed) {
+		foreach(GameObject go in wobbleBlocks)
+		{
+			if(angle)
 			{
-				if(angle)
-				{
-					go.transform.RotateAroundLocal(new Vector3(0,0,1), 3.0f * Time.fixedDeltaTime);	
-					ang += 3.0f * Time.fixedDeltaTime;
-				}
-				else
-				{
-					go.transform.RotateAroundLocal(new Vector3(0,0,1), -3.0f * Time.fixedDeltaTime);	
-					ang -= 3.0f * Time.fixedDeltaTime;
-				}
+				go.transform.RotateAroundLocal (Vector3.forward, speed * Time.fixedDeltaTime);
+				ang += speed * Time.fixedDeltaTime;
 			}
-			if(ang >= 30.0f)
+			else
+			{
+				go.transform.RotateAroundLocal (Vector3.forward, -speed * Time.fixedDeltaTime);
+				ang -= speed * Time.fixedDeltaTime;
+			}
+			if(ang >= maxAngle)
 				angle = false;
-			else if(ang <= -30.0f)
+			else if(ang <= -maxAngle)
 				angle = true;
 		}
+	}
+	
+	///<summary>
+	///Shakes the camera a certain distance and speed
+	///</summary>
+	///<param name="displacement">
+	///The maximum distance the camera can move
+	///</param>
+	///<param name="speed">
+	///How fast the camera will move
+	///</param>
+	void ShakeCamera (float displacement, float speed) {
+		if(direction)
+		{
+			mainCamera.transform.Translate (Vector3.up * speed * Time.fixedDeltaTime);
+			distance += speed * Time.fixedDeltaTime;
+			Debug.Log ("Distance " + distance);
+		}
+		else
+		{
+			mainCamera.transform.Translate (Vector3.up * -speed * Time.fixedDeltaTime);
+			distance -= speed * Time.fixedDeltaTime;
+		}
+		if(distance >= displacement)
+			direction = false;
+		else if(distance <= -displacement)
+			direction = true;
 	}
 }
